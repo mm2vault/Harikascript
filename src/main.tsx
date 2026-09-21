d24a1ebd539d122119a08ba72fcacd2f0b803cc8
mm2vault <huseynoveleddin87@gmail.com>
2026-09-22 02:07:32 +0400

    Protect app from runtime crashes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import { AppErrorBoundary } from './AppErrorBoundary';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AppErrorBoundary>\n      <App />\n    </AppErrorBoundary>
   </StrictMode>,
 );
