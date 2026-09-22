dcef00b611f8cafb845e01c8beec14a509c44ca5
mm2vault <huseynoveleddin87@gmail.com>
2026-09-22 16:17:43 +0400

    feat: add Firebase client setup

---
+++ src/lib/firebase.ts
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyB7YTDtS5Uyetoh4X08QDkEAebRbH1BaCk',
+  authDomain: 'harikascript.firebaseapp.com',
+  projectId: 'harikascript',
+  storageBucket: 'harikascript.firebasestorage.app',
+  messagingSenderId: '728900819773',
+  appId: '1:728900819773:web:a7c221c180f97139b04354',
+  measurementId: 'G-XWR2H21W9L'
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
+export const firebaseDb = getFirestore(firebaseApp);
+export const firebaseStorage = getStorage(firebaseApp);
+export const isFirebaseConfigured = true;
